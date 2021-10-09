aa5f8a0117d5a070c69a56bcb6e4002301705b44
Daniel Vejar <albinovejar@hotmail.com>
2021-10-08 18:27:09 -0700

    Add new logic in form

--- src/app/views/sign-up/student-main-data/student-main-data.component.ts
+++ src/app/views/sign-up/student-main-data/student-main-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-main-data',
@@ -8,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class StudentMainDataComponent implements OnInit {
 
   constructor() { }
-
+  fromStudent = new FormGroup({
+    yearSchool: new FormControl(''),
+    campus: new FormControl(''),
+    section: new FormControl(''),
+    grade: new FormControl(''),
+    studentData: new FormGroup({
+      lastFirstName: new FormControl(''),
+      lastSecondName: new FormControl(''),
+      names: new FormControl(''),
+      sex: new FormControl(''),
+      bornDate: new FormGroup({
+        day: new FormControl(''),
+        month: new FormControl(''),
+        year: new FormControl(''),
+      }),
+      bornPlace: new FormControl(''),
+      address: new FormControl(''),
+      col: new FormControl(''),
+      postalCode: new FormControl(''),
+      phone: new FormControl(''),
+      curp: new FormControl(''),
+      height: new FormControl(''),
+      weight: new FormControl(''),
+      bloodType: new FormControl(''),
+      glasses: new FormControl(''),
+      alergic: new FormControl(''),
+    })
+  });
   ngOnInit() {
   }
 
