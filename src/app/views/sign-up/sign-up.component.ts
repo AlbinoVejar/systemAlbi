cc20048d9baad367754685a51ddfc37e68f72a76
Daniel Vejar <albinovejar@hotmail.com>
2021-10-26 23:08:13 -0700

    update sign-up

--- src/app/views/sign-up/sign-up.component.ts
+++ src/app/views/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { FormGroup } from '@angular/forms';
+import { AlertService } from 'src/services/alert.service';
+import { FirebaseService } from 'src/services/firebase.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,15 +15,27 @@ import { FormGroup } from '@angular/forms';
   { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }]
 })
 export class SignUpComponent implements OnInit {
-
+  studentForm: FormGroup = new FormGroup({});
   mainForm = new FormGroup({
   });
-  constructor() { }
+  constructor(
+    private serviceFirebase: FirebaseService,
+    private serviceAlerts: AlertService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   getFormStudentData(data: any){
-    
+    console.log(data);
+  }
+
+  private async postStudent(data: any){
+    const result = await this.serviceFirebase.storeStudent(data);
+    if(result){
+      this.serviceAlerts.ShowToastSuccess('Exito','Alumno Registrado con Exito');
+    }else{
+      this.serviceAlerts.ErrorAlert('Error', 'Ocurrió un error al guardar');
+    }
   }
 }
