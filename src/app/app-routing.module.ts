7426199aadf848d0805f8c0f9fbc5179cc8235d2
Daniel Vejar <albinovejar@hotmail.com>
2021-10-26 23:08:00 -0700

    add sweetalert & theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignUpComponent } from './views/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./views/views-routing.module').then(m => m.ViewsRoutingModule) },
