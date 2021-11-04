c3376c2807e5be88de5db336a405076adb3c2174
Daniel Vejar <albinovejar@hotmail.com>
2021-11-03 20:35:27 -0700

    create a form group to get info from view

--- src/app/views/sign-up/student-ailments/student-ailments.component.ts
+++ src/app/views/sign-up/student-ailments/student-ailments.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-ailments',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-ailments.component.scss']
 })
 export class StudentAilmentsComponent implements OnInit {
-
+  @Output() setAilmentsGroup = new EventEmitter<FormGroup>();
+  formAilments = new FormGroup({
+    isDeficit: new FormControl(null, Validators.required),
+    useMedicine: new FormControl(null, Validators.required),
+    medicines: new FormControl(null),
+    infomationAboutStudent: new FormControl(null),
+  });
+  formStudentComplement = new FormGroup({
+    lastSchoolName: new FormControl(null, Validators.required),
+    brothersNames: new FormControl(null),
+  })
+  formParents = new FormGroup({
+    fatherData: new FormGroup({
+      names: new FormControl(null),
+      mainPhoneNumber: new FormControl(null),
+      phoneNumber: new FormControl(null),
+      address: new FormControl(null),
+      colony: new FormControl(null),
+      postalCode: new FormControl(null),
+      state: new FormControl(null),
+      city: new FormControl(null),
+      job: new FormControl(null),
+      workPlace: new FormControl(null),
+      workPhone: new FormControl(null),
+      schedule: new FormControl(null)
+    })
+  })
   constructor() { }
 
   ngOnInit() {
