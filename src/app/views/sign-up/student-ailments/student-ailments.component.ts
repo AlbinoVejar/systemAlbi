2bfb075d4e5e0b2f82e1ed1f152d4b457f872283
Daniel Vejar <albinovejar@hotmail.com>
2021-11-04 00:00:37 -0700

    get all value in form

--- src/app/views/sign-up/student-ailments/student-ailments.component.ts
+++ src/app/views/sign-up/student-ailments/student-ailments.component.ts
@@ -4,7 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-student-ailments',
   templateUrl: './student-ailments.component.html',
-  styleUrls: ['./student-ailments.component.scss']
+  styleUrls: ['./student-ailments.component.scss'],
 })
 export class StudentAilmentsComponent implements OnInit {
   @Output() setAilmentsGroup = new EventEmitter<FormGroup>();
@@ -12,31 +12,55 @@ export class StudentAilmentsComponent implements OnInit {
     isDeficit: new FormControl(null, Validators.required),
     useMedicine: new FormControl(null, Validators.required),
     medicines: new FormControl(null),
-    infomationAboutStudent: new FormControl(null),
-  });
-  formStudentComplement = new FormGroup({
-    lastSchoolName: new FormControl(null, Validators.required),
-    brothersNames: new FormControl(null),
-  })
-  formParents = new FormGroup({
-    fatherData: new FormGroup({
-      names: new FormControl(null),
-      mainPhoneNumber: new FormControl(null),
-      phoneNumber: new FormControl(null),
-      address: new FormControl(null),
-      colony: new FormControl(null),
-      postalCode: new FormControl(null),
-      state: new FormControl(null),
-      city: new FormControl(null),
-      job: new FormControl(null),
-      workPlace: new FormControl(null),
-      workPhone: new FormControl(null),
-      schedule: new FormControl(null)
+    informationAboutStudent: new FormControl(null),
+    formStudentComplement: new FormGroup({
+      lastSchoolName: new FormControl(null, Validators.required),
+      brothersNames: new FormControl(null),
+    }),
+    formParents: new FormGroup({
+      fatherData: new FormGroup({
+        names: new FormControl(null),
+        mainPhoneNumber: new FormControl(null),
+        phoneNumber: new FormControl(null),
+        address: new FormControl(null),
+        colony: new FormControl(null),
+        postalCode: new FormControl(null),
+        state: new FormControl(null),
+        city: new FormControl(null),
+        workData: new FormGroup({
+          jobDescription: new FormControl(null),
+          workPlace: new FormControl(null),
+          workPhone: new FormControl(null),
+          schedule: new FormControl(null),
+        }),
+      }),
+      motherData: new FormGroup({
+        names: new FormControl(null),
+        mainPhoneNumber: new FormControl(null),
+        phoneNumber: new FormControl(null),
+        address: new FormControl(null),
+        colony: new FormControl(null),
+        postalCode: new FormControl(null),
+        state: new FormControl(null),
+        city: new FormControl(null),
+        workData: new FormGroup({
+          jobDescription: new FormControl(null),
+          workPlace: new FormControl(null),
+          workPhone: new FormControl(null),
+          schedule: new FormControl(null),
+        }),
+      }),
     })
-  })
-  constructor() { }
+  });
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public next() {
+    if(this.formAilments.valid){
+      this.setAilmentsGroup.emit(this.formAilments);
+    }else{
+      this.formAilments.markAllAsTouched();
+    }
   }
-
 }
