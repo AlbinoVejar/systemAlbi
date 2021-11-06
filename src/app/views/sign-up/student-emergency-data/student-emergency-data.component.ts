f3702b8a4b09a729ab52acdd06d81acd6f831a02
Daniel Vejar <albinovejar@hotmail.com>
2021-11-05 23:25:51 -0700

    update component

--- src/app/views/sign-up/student-emergency-data/student-emergency-data.component.ts
+++ src/app/views/sign-up/student-emergency-data/student-emergency-data.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-emergency-data',
@@ -10,12 +10,32 @@ export class StudentEmergencyDataComponent implements OnInit {
   @Output() setEmergencyGroup = new EventEmitter<FormGroup>();
   formEmergency = new FormGroup({
     family1: new FormGroup({
-      
+      name: new FormControl(null),
+      typeFamily: new FormControl(null),
+      phone: new FormControl(null),
+    }),
+    family2: new FormGroup({
+      name: new FormControl(null),
+      typeFamily: new FormControl(null),
+      phone: new FormControl(null),
+    }),
+    family3: new FormGroup({
+      name: new FormControl(null),
+      typeFamily: new FormControl(null),
+      phone: new FormControl(null),
     })
   });
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public next() {
+    if(this.formEmergency.valid){
+      this.setEmergencyGroup.emit(this.formEmergency);
+    }else{
+      this.formEmergency.markAllAsTouched();
+    }
   }
 
 }
