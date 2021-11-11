d540df1e75177fdd60fee8bcf2d5667e8df6dc5e
Daniel Vejar <albinovejar@hotmail.com>
2021-11-10 22:19:15 -0700

    card component update

--- src/app/views/sign-up/student-card-data/student-card-data.component.ts
+++ src/app/views/sign-up/student-card-data/student-card-data.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-card-data',
@@ -8,9 +8,22 @@ import { FormGroup } from '@angular/forms';
 })
 export class StudentCardDataComponent implements OnInit {
   @Output() setStudentCardGroup = new EventEmitter<FormGroup>();
+  formStudentCard = new FormGroup({
+    tutor: new FormControl(''),
+    phoneFather: new FormControl(''),
+    phoneMother: new FormControl(''),
+    phoneGrandparents: new FormControl('')
+  });
   constructor() { }
 
   ngOnInit() {
   }
 
+  next(){
+    if(this.formStudentCard.valid){
+      this.setStudentCardGroup.emit(this.formStudentCard);
+    }else{
+      this.formStudentCard.markAllAsTouched();
+    }
+  }
 }
