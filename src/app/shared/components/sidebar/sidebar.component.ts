160b86d0ce363af45760ce5947c2fa8d49f8419c
Daniel Vejar <albinovejar@hotmail.com>
2021-11-29 22:55:33 -0700

    update sidebar & path

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SideBarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
