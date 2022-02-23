b4a1902cbfb4a9c20c5653733b7108134fd59108
Albino Vejar <albinovejar@hotmail.com>
2022-02-22 23:27:52 -0700

    update page students

--- src/app/pages/cathalogs/students/students.component.ts
+++ src/app/pages/cathalogs/students/students.component.ts
@@ -1,4 +1,8 @@
+import { ModalStudentComponent } from './components/modal-student/modal-student.component';
+import { Student } from './../../../../models/student';
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/services/student.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
-
-  constructor() { }
+  lstStudents: Array<Student> = [];
+  constructor(
+    private serviceStudent: StudentService,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+    this.GetStudents();
   }
 
+  private async GetStudents(){
+    const result = await this.serviceStudent.GetStudents();
+    console.log(result);
+    this.lstStudents = await result;
+  }
+
+  public showModal(){
+    const modalRef = this.modalService.open(ModalStudentComponent, {centered: true, size: 'xl'});
+    modalRef.componentInstance.title = "Ver Detalles"
+  }
 }
