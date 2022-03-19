f8a442fb2b686b693f98319b3a9bb8521126d0dc
Albino Vejar <albinovejar@hotmail.com>
2022-03-18 22:56:15 -0700

    two generate pdfs

---
+++ src/services/pdfs/carnet.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarnetService {
+
+  constructor() { }
+
+  createCarnet() {
+    const doc = new jsPDF({
+      orientation: 'l',
+      unit: 'mm',
+      format: 'letter'
+    });
+
+    const height = doc.internal.pageSize.getHeight(); //215.9
+    const width = doc.internal.pageSize.getWidth(); //279.4 
+    let y = 15;
+    doc.setFontSize(10)
+    doc.text("CICLO ESCOLAR", 200, y);
+    y += 10;
+    doc.text("NI", 200, y);
+    y += 10;
+    doc.text("FECHA DE NACIMIENTO: 9/21/2009", 200, y);
+    y += 10;
+    doc.text("NOMBRE DEL ALUMNO: ", 10, y);
+    doc.text("SECCIÓN: SECUNDARIA", 100, y);
+    doc.text("GRADO: 1ERO", 180, y);
+    doc.text("GRUPO:", 240, y);
+    y += 8;
+    doc.text("Nombre del padre/madre o tutor: ERIK FLORES FERNANDEZ", 10, y);
+    doc.text("TEL. 1: 6623867266", 150, y);
+    doc.text("TEL. 2: 6623867266", 200, y);
+    y += 5;
+    doc.text("TEL. 3: 6623867266", 10, y);
+    doc.text("ABUELOS", 60, y);
+    doc.text("PAPÁ", 155, y);
+    doc.text("MAMÁ", 200, y);
+
+    y += 8;
+    doc.text("COLEGIATURA A 12 MESES: $", 10, y);
+    doc.text("A PARTIR DEL DIA ONCE DE CADA MES SE COBRARÁ INTERÉS DEL $220.00", 90, y);
+    y += 5;
+    doc.text("COLEGIATURA A 10 MESES: $", 10, y);
+    doc.text("A PARTIR DEL DIA ONCE DE CADA MES SE COBRARÁ INTERÉS DEL $220.00", 90, y);
+
+    y += 10;
+    doc.text("COLEGIATURAS", 40, y);
+    y += 4;
+    const e = doc.splitTextToSize("Las colegiaturas deberán cubrirse dentro de los primeros 10 (diez) días de cada mes.Se aplicará un recargo de $220.00 en los pagos que se realicen después de este día haciéndose efectivo a partir del día 11 (once).", 250);
+    doc.text(e, 134, y, { align: 'center' });
+    y += 20;
+    const warning = doc.splitTextToSize("Si no se ha cubierto para el día 20 (veinte) del mes, nos veremos en la penosa necesidad de negar el derecho de exámenes a su hijo (a) y le negaremos la entrada a clases. Por favor, no permita que esto suceda.", 250);
+    doc.text(warning, 134, y, { align: 'center' });
+
+    y += 20;
+    doc.text("BLVD. JOSE MARIA ESCRIBA No. 143 ENTRE CALLES LUCRECIA RUIZ Y LUZ VALENCIA", 134, y, { align: 'center' });
+    y += 5;
+    doc.text("COL. VILLAS DEL PALMAR C.P. 83105", 100, y);
+    y += 5;
+    doc.text("TEL. (662) 2 80 80 81 HERMOSILLO, SONORA", 95, y);
+
+    // const blob = new Blob([doc.output('blob')], { type: 'application/pdf' });
+    // const blobUrl = URL.createObjectURL(blob);
+    // pdfObject.embed(blobUrl, this.input);
+  }
+}
---
+++ src/services/pdfs/constancia.service.ts
@@ -0,0 +1,178 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConstanciaService {
+
+  constructor() { }
+
+  createFolio() {
+    const doc = new jsPDF({
+      orientation: 'p',
+      unit: 'mm',
+      format: 'letter'
+    });
+
+    const height = doc.internal.pageSize.getHeight(); //215.9
+    const width = doc.internal.pageSize.getWidth(); //279.4 
+    let y = 15;
+    // let img = new Image();
+    // img.src = "../assets/logo.png"
+    // img.crossOrigin = "anonymous";
+    // let result: string;
+    // var canvas = this.input?.elementRef.nativeElement;
+    // var ctx = canvas.getContext("2d");
+    // img.onload = () => {
+    //   ctx?.drawImage(img,512,182);
+    //   return canvas;
+    // }
+    // doc.addImage(canvas.toDataURL('image/png'),'PNG',10, y, 100, 40); 
+    doc.setFontSize(14);
+    doc.setFont("helvetica", "bold");
+    doc.text("SOLICITUD A: SECUNDARIA", 120, y);
+    y += 6;
+    doc.setFontSize(14);
+    doc.setFont("helvetica", "normal");
+    doc.text("Grado a cursar: ", 120, y);
+    doc.setTextColor("blue");
+    doc.text("1ERO", 155, y)
+    y += 6;
+    doc.setTextColor("black"); "./../assets/logo.png";
+    doc.text("Ciclo Escolar: ", 120, y);
+    doc.setTextColor("blue");
+    doc.text("2021-2022", 155, y)
+
+    y += 6;
+    doc.setFontSize(10)
+    doc.setTextColor("black");
+    doc.text("Nombre del alumno (a)", 10, y);
+    doc.line(48, y + 1, 206, y + 1);
+    y += 6;
+    doc.text("Fecha de nacimiento", 10, y);
+    doc.line(45, y, 117, y);
+    doc.text("Lugar", 120, y);
+    doc.line(130, y, 206, y);
+    y += 6;
+    doc.text("Dirección", 10, y);
+    doc.line(27, y, 206, y);
+    y += 6;
+    doc.text("Colonia", 10, y);
+    doc.line(24, y, 98, y);
+    doc.text("C.P.", 100, y);
+    doc.line(108, y, 130, y);
+    doc.text("Teléfono Particular", 132, y);
+    doc.line(163, y, 206, y);
+    y += 6;
+    doc.text("CURP", 10, y);
+    doc.line(21, y, 98, y);
+    doc.text("Estatura", 100, y);
+    doc.line(115, y, 158, y);
+    doc.text("Peso", 160, y);
+    doc.line(170, y, 206, y);
+    y += 6;
+    doc.text("Tipo de Sangre", 10, y);
+    doc.line(35, y, 63, y);
+    doc.text("Utiliza Lentes", 65, y);
+    doc.line(88, y, 108, y);
+    doc.text("Alérgico a:", 110, y);
+    doc.line(128, y, 206, y);
+
+    y += 10;
+    doc.text("PADECIMIENTOS:", 10, y);
+    y += 6;
+    doc.text("Déficit de atención e hiperactividad:", 10, y);
+    doc.line(67, y, 148, y);
+    doc.text("Medicado", 150, y);
+    doc.line(167, y, 206, y);
+    y += 6;
+    doc.text("Medicamentos", 10, y);
+    doc.line(35, y, 206, y);
+    y += 6;
+    doc.text("Alguna cosa que nosotros debamos saber para ayudar a su hijo (a):", 10, y);
+    doc.rect(10, y + 2, 196, 12);
+    y += 20;
+    doc.text("Nombre del Padre o Tutor", 10, y);
+    doc.line(53, y, 206, y);
+    y += 6;
+    doc.text("Celular", 10, y);
+    doc.line(23, y, 98, y);
+    doc.text("E-mail", 100, y);
+    doc.line(112, y, 206, y);
+    y += 6;
+    doc.text("Ocupación", 10, y);
+    doc.line(29, y, 98, y);
+    doc.text("Lugar de Trabajo", 100, y);
+    doc.line(129, y, 206, y);
+    y += 6;
+    doc.text("Horario", 10, y);
+    doc.line(23, y, 98, y);
+    doc.text("Teléfono(s) trabajo", 100, y);
+    doc.line(132, y, 206, y);
+    y += 6;
+    doc.text("Nombre de la Madre", 10, y);
+    doc.line(44, y, 206, y);
+    y += 6;
+    doc.text("Celular", 10, y);
+    doc.line(23, y, 98, y);
+    doc.text("E-mail", 100, y);
+    doc.line(112, y, 206, y);
+    y += 6;
+    doc.text("Ocupación", 10, y);
+    doc.line(29, y, 98, y);
+    doc.text("Lugar de Trabajo", 100, y);
+    doc.line(129, y, 206, y);
+    y += 6;
+    doc.text("Horario", 10, y);
+    doc.line(23, y, 98, y);
+    doc.text("Teléfono(s) trabajo", 100, y);
+    doc.line(132, y, 206, y);
+    y += 8;
+    doc.text("Familiar (es) a quien (es) llamar en caso de emergencia:", 10, y);
+    y += 2;
+    const data: Array<RowInput> = [{ name: "Example", parent: "Example", tel: "example eaera" }, { name: "Example", parent: "Example", tel: "example eaera" }, { name: "Example", parent: "Example", tel: "example eaera" }];
+    const headers: Array<ColumnInput> = [{ header: "Nombre", dataKey: "name" }, { header: "Parentesco", dataKey: "parent" }, { header: "Telefono", dataKey: "tel" }]
+
+    autoTable(doc, { startY: y, margin: { left: 10, right: 10 }, columns: headers, body: data, theme: 'striped', styles: { lineColor: 'black', lineWidth: 0.2, halign: 'center', textColor: 'blue' }, headStyles: { fillColor: 'white', textColor: 'black', fontStyle: 'normal' } });
+
+    y = (doc as any).lastAutoTable.finalY;
+
+    y += 3;
+    doc.setDrawColor('black');
+    doc.setLineWidth(.5);
+    doc.rect(10, y, 196, 40, 'S');
+    y += 6;
+    doc.text("DOCUMENTOS PRESENTADOS", 14, y);
+    y += 4;
+    doc.setFontSize(10);
+    doc.rect(15, y, 8, 5, 'S');
+    doc.text("CERTIFICADO DE PRIMARIA", 25, y + 4);
+    doc.rect(85, y, 8, 5, 'S');
+    doc.text("COPIA DE CURP", 95, y + 4);
+    doc.rect(140, y, 8, 5, 'S');
+    doc.text("CARTA DE BUENA CONDUCTA", 150, y + 4);
+    y += 10;
+    doc.rect(15, y, 8, 5, 'S');
+    doc.text("ULTIMA BOLETA", 25, y + 4);
+    doc.rect(85, y, 8, 5, 'S');
+    doc.text("ACTA DE NACIMIENTO", 95, y + 4);
+    doc.rect(140, y, 8, 5, 'S');
+    doc.text("CARTA DE NO ADEUDO", 150, y + 4);
+    y += 10;
+    doc.rect(15, y, 8, 5, 'S');
+    doc.text("FOTOGRAFÍAS", 25, y + 4);
+
+    y += 16;
+    doc.text("HERMOSILLO, SONORA a: Sábado, Marzo 12, 2020", 68, y);
+
+    y += 25;
+    doc.line(15, y - 5, 86, y - 5);
+    doc.text("FIRMA DEL INTERESADO", 30, y);
+    doc.line(125, y - 5, 200, y - 5);
+    doc.text("FIRMA DEL COORDINADOR", 140, y);
+
+    // const blob = new Blob([doc.output('blob')], {type: 'application/pdf'});
+    // const blobUrl = URL.createObjectURL(blob);
+    // pdfObject.embed(blobUrl, this.input);
+  }
+}
