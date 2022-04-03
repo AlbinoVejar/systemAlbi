ede24350636dfb1896f951e5ab91a0c2cdd26951
Daniel Vejar <albinovejar@hotmail.com>
2022-04-02 17:08:16 -0700

    now init app get catalogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CathalogService } from 'src/services/cathalog.service';
+import { CustomStateService } from 'src/services/customState.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'argon-dashboard-angular';
+  constructor(
+    private serviceLocal: CustomStateService,
+    private serviceCatalogs: CathalogService,
+  ) {
+    this.InitCatalogs();
+  }
+
+  private InitCatalogs(){
+    this.serviceCatalogs.GetCatalogs().then((result: any) => {
+      this.serviceLocal.SaveItem("_state", JSON.stringify(result));
+    });
+  }
 }
